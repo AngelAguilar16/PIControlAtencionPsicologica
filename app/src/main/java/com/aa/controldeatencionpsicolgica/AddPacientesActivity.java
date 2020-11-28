3fd94e835d857096730ccf175ae3a8f5a717733d
Luis Fernando <fernando04_software@outlook.com>
2020-11-28 13:47:24 -0600

    Renombramiento de Activity
    Jajaja perdon

--- app/src/main/java/com/aa/controldeatencionpsicolgica/AddPacientesActivity.java
+++ app/src/main/java/com/aa/controldeatencionpsicolgica/AddPacientesActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AddPacientes extends AppCompatActivity {
+public class AddPacientesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
