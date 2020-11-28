24ea1e829a558e4c2abd4d882ecc8293a301a1fd
Luis Fernando <fernando04_software@outlook.com>
2020-11-28 16:35:50 -0600

    Add files - AddPacientesActivity (xml & java)
    Se añaden archivos para añadir pacientes, el xml ya contiene los elementos.

---
+++ app/src/main/java/com/aa/controldeatencionpsicolgica/AddPacientesActivity.java
@@ -0,0 +1,14 @@
+package com.aa.controldeatencionpsicolgica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddPacientesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_pacientes);
+    }
+}
