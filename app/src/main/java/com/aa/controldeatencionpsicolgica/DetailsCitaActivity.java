8b9c5d042ae01196d4940ec642b880266bc221d4
Luis Fernando <fernando04_software@outlook.com>
2021-02-03 02:40:24 -0600

    New Files - Agregar Pacientes

---
+++ app/src/main/java/com/aa/controldeatencionpsicolgica/DetailsCitaActivity.java
@@ -0,0 +1,14 @@
+package com.aa.controldeatencionpsicolgica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailsCitaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_cita);
+    }
+}
