9ae844f4ce0a31b3493a53df5878ee2a2c5b1196
Luis Fernando <fernando04_software@outlook.com>
2021-02-04 00:09:01 -0600

    Auto stash before merge of "master" and "origin/master"
    Conflictos

--- app/src/main/java/com/aa/controldeatencionpsicolgica/UsuariosTransActivtiy.java
+++ app/src/main/java/com/aa/controldeatencionpsicolgica/UsuariosTransActivtiy.java
@@ -2,13 +2,69 @@ package com.aa.controldeatencionpsicolgica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.aa.controldeatencionpsicolgica.Adapter.Pacientes_Adapter;
+import com.aa.controldeatencionpsicolgica.Global.Global;
+import com.aa.controldeatencionpsicolgica.Handlers.Handler;
+import com.aa.controldeatencionpsicolgica.Model.Paciente;
+import com.aa.controldeatencionpsicolgica.Model.Usuario;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
 
 public class UsuariosTransActivtiy extends AppCompatActivity {
+
+    private ListView lvUsuariosTrans;
+    private List<Usuario> usuarioList;
+
+    private int id_user, id_paciente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usuarios_trans_activtiy);
+
+        lvUsuariosTrans = findViewById(R.id.lvUsuariosTrans);
+        id_paciente = getIntent().getIntExtra("id_paciente", 0);
+
+        lvUsuariosTrans.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Usuario usuario = usuarioList.get(position);
+
+            }
+        });
+
     }
+
+    /*private void showList(){
+        StringRequest stringRequest = new StringRequest(Global.ip + "listPacientes.php?usuario="+ Global.us, response -> {
+            try {
+                JSONObject obj = new JSONObject(response);
+                JSONArray array = obj.getJSONArray("pacientesList");
+                for (int i = 0; i < array.length(); i++){
+                    JSONObject pacObj = array.getJSONObject(i);
+                    Usuario p = new Usuario();
+                    usuarioList.add(p);
+                }
+                // Crear Usuario Adapter
+                Pacientes_Adapter adapter = new Pacientes_Adapter(usuarioList, getApplicationContext());
+                lvUsuariosTrans.setAdapter(adapter);
+                //Toast.makeText(AgendaActivity.this,"Funcion Activada" + Global.us,Toast.LENGTH_LONG).show();
+            } catch (JSONException e) {
+                //Toast.makeText(AgendaActivity.this,"Funcion No Jalo " + e,Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            }
+        }, error -> { });
+        Handler.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
+    }*/
 }
